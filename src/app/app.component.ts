8d82a56687e773d0635b0529b5be0dae0e17b01e
Eduard <jimlab@mail.ru>
2019-12-25 16:02:49 +0300

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,17 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs';
+import {AppCounterService} from './services/app-counter.service';
+import {LocalCounterService} from './services/local-counter.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService]
 })
 export class AppComponent {
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolved');
-    }, 4000);
-  });
-  date: Observable<Date> = new Observable<Date>(obs => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
+  constructor(
+    private appCounterService: AppCounterService,
+    private localCounterService: LocalCounterService
+  ) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,19 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {MultByPipe} from './pipes/mult-by.pipe';
-import {ExMarksPipe} from './pipes/ex-marks.pipe';
-import {FilterPipe} from './pipes/filter.pipe';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe,
-    ExMarksPipe,
-    FilterPipe
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
