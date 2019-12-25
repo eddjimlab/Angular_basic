efa30d34657af89958368943aca4bd16827e4d68
Eduard <jimlab@mail.ru>
2019-12-25 14:55:52 +0300

    pipes complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
 
-export interface Post {
-  title: string;
-  text: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -11,18 +8,14 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: Post[] = [
-    {title: 'Beer', text: 'Best beer in the world'},
-    {title: 'Bread', text: 'Best bread in the world'},
-    {title: 'Javascript', text: 'Best language in the world'},
-  ];
-  search: '';
-  searchField: 'title';
-
-  addPost() {
-    this.posts.unshift({
-      title: 'Angular 8 course',
-      text: 'Eduard Angular 8 basics'
-    });
-  }
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolved');
+    }, 4000);
+  });
+  date: Observable<Date> = new Observable<Date>(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
 }
