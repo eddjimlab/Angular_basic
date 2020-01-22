6985dafb8505d8c7a948b6df6db8c716c3ba63d8
Eduard <jimlab@mail.ru>
2020-01-22 14:59:10 +0300

    angular animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, group, keyframes, query, state, style, transition, trigger} from '@angular/animations';
+import {boxAnimation} from './animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [boxAnimation]
 })
 export class AppComponent {
-  title = 'angular-unit-tests';
+  boxState = 'start';
+  visible = true;
+
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end';
+  }
+
+  animationStart(event: AnimationEvent) {
+    console.log('Animation start', event);
+  }
+
+  animationDone(event: AnimationEvent) {
+    console.log('Animation done', event);
+  }
 }
